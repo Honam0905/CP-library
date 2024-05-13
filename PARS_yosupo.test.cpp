8c336b14de55cc2b3d1539e98538eb752bbaad3e
Kiffaz0915 <138308525+Kiffaz0915@users.noreply.github.com>
2024-05-13 08:45:46 +0700

    Update PARS_yosupo.test.cpp

--- PARS_yosupo.test.cpp
+++ PARS_yosupo.test.cpp
@@ -4,26 +4,6 @@
 #include "fast_io.hpp"
 #include "Fenwick_tree.hpp"
 
-void solve()
-{
-    int n,q; sc.read(n,q);
-    vector<ll> a(n);
-    rep(i,n) sc.read(a[i]);
-    Fenwick ft(n);
-    rep(i,n) ft.update(i+1,a[i]);
-    while (q--) {
-        int op; sc.read(op);
-        if (op == 0) {
-            int p, x; sc.read(p,x);
-            ft.update(p + 1, x);
-        } else {
-            int l, r; sc.read(l,r);
-            pr.write(ft.prefix(l,r));
-            pr.writeln();
-        }
-    }
-}
-
 int main(){
 FT;
 int t=1; 
@@ -32,7 +12,21 @@ while(t--)
 {
 //tc++;
 //cout << "Case #" << tc << ": ";
-solve();
+int n,q; cin>>n>>q;
+    vector<ll> a(n);
+    rep(i,n) cin>>a[i];
+    Fenwick ft(n);
+    rep(i,n) ft.update(i+1,a[i]);
+    while (q--) {
+        int op; cin>>op;
+        if (op == 0) {
+            int p, x; cin>>p>>x;
+            ft.update(p + 1, x);
+        } else {
+            int l, r; sc.read(l,r);
+            cout<<ft.prefix(l,r)<<'\n';
+        }
+    }
 // cerr << "Time elapsed: " << TIME << " s.\n";
 }
 }
