7520d43e0b2ead04615a64949713d7cadde8c234
honam <138308525+Honam0905@users.noreply.github.com>
2024-05-18 22:38:20 +0700

    Update Segment_tree.hpp

--- DS/Segment_tree.hpp
+++ DS/Segment_tree.hpp
@@ -21,23 +21,19 @@ public:
         }
     }
 
-    void build() {
-        for (int i = n - 1; i > 0; --i)
-            t[i] = op(t[i << 1], t[i << 1 | 1]);
+    void build() { //not necessary
+        for (int i = n - 1; i > 0; --i) t[i] = op(t[i << 1], t[i << 1 | 1]);
     }
 
     void modify(int p, T value) {
-        for (t[p += n] = value; p > 1; p >>= 1)
-            t[p >> 1] = op(t[p], t[p ^ 1]);
+        for (t[p += n] = value; p > 1; p >>= 1)  t[p >> 1] = op(t[p], t[p ^ 1]);
     }
 
     T query(int l, int r) {
         T res = e();
         for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
-            if (l & 1)
-                res = op(res, t[l++]);
-            if (r & 1)
-                res = op(res, t[--r]);
+            if (l & 1) res = op(res, t[l++]);
+            if (r & 1) res = op(res, t[--r]);
         }
         return res;
     }
