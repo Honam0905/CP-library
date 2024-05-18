6c5253b87d4b40fee553e4535206dd1f663f0422
honam <138308525+Honam0905@users.noreply.github.com>
2024-05-18 23:16:59 +0700

    Update Segment_tree.hpp

--- DS/Segment_tree.hpp
+++ DS/Segment_tree.hpp
@@ -3,34 +3,30 @@ template <class T> class SegTree {
 private:
     int n;  // array size
     vector<T> t;
-
-    T op(T a, T b) { //operation 
-        return a+b;  
-    }
-
-    T e() { // modify depend on the problem
-        return 0;  
-    }
+    T (*op)(T, T);
+    T (*e)();
 
 public:
-    SegTree(int size) {
+    SegTree(int size, T (*op)(T, T), T (*e)()) {
         n = size;
+        this->op = op;
+        this->e = e;
         t.resize(4 * n);
         for (int i = 0; i < 4 * n; i++) {
-            t[i] = e();
+            t[i] = this->e();
         }
     }
 
-    void build() { //not necessary
+    void build() {
         for (int i = n - 1; i > 0; --i) t[i] = op(t[i << 1], t[i << 1 | 1]);
-    }
-
-    void add(int p, T value) {
-        for (t[p += n] += value; p > 1; p >>= 1)  t[p >> 1] = op(t[p], t[p ^ 1]);
     }
 
     void modify(int p, T value) {
         for (t[p += n] = value; p > 1; p >>= 1)  t[p >> 1] = op(t[p], t[p ^ 1]);
+    }
+
+    void add(int p, T value) {
+        for (t[p += n] += value; p > 1; p >>= 1)  t[p >> 1] = op(t[p], t[p ^ 1]);
     }
 
     T query(int l, int r) {
