23fa2fd767c80c486386fe47378c532da1136e75
honam <138308525+Honam0905@users.noreply.github.com>
2024-05-18 22:36:46 +0700

    Update and rename Segment_tree.hpp to DS/Segment_tree.hpp

---
+++ DS/Segment_tree.hpp
@@ -0,0 +1,44 @@
+#pragma once
+template <class T> class SegTree {
+private:
+    int n;  // array size
+    vector<T> t;
+
+    T op(T a, T b) { //operation 
+        return min(a, b);  
+    }
+
+    T e() { // modify depend on the problem
+        return INT_MAX;  
+    }
+
+public:
+    SegTree(int size) {
+        n = size;
+        t.resize(4 * n);
+        for (int i = 0; i < 4 * n; i++) {
+            t[i] = e();
+        }
+    }
+
+    void build() {
+        for (int i = n - 1; i > 0; --i)
+            t[i] = op(t[i << 1], t[i << 1 | 1]);
+    }
+
+    void modify(int p, T value) {
+        for (t[p += n] = value; p > 1; p >>= 1)
+            t[p >> 1] = op(t[p], t[p ^ 1]);
+    }
+
+    T query(int l, int r) {
+        T res = e();
+        for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
+            if (l & 1)
+                res = op(res, t[l++]);
+            if (r & 1)
+                res = op(res, t[--r]);
+        }
+        return res;
+    }
+};
