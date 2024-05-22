4631d9b25c5220feed5b23824ac46129d809f8b4
honam <138308525+Honam0905@users.noreply.github.com>
2024-05-22 09:05:10 +0700

    Create BC_prime_mod.test.cpp

---
+++ test/yosupo/Math/BC_prime_mod.test.cpp
@@ -0,0 +1,19 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/binomial_coefficient_prime_mod"
+#include "Misc/marco.hpp"
+#include "Misc/debug.hpp"
+const int INF=1e9;
+const ll INFI=1e15;
+//----------Author: Nguyen Ho Nam,UIT, Saigon-----------------
+#include "Modint/dynamic_modint.hpp"
+#include "Math/Combinatorics/Comb_common.hpp"
+using mint=modint;
+int main() {
+    ll tt,m; cin>>tt>>m;
+    mint::set_mod(m);
+    Comb<mint>com;
+    while(tt--){
+        ll n,k; cin>>n>>k;
+        cout<<com.binom(n,k).get()<<'\n';
+    }
+    return 0;
+}
