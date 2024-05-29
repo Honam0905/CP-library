3049541f9efeef5eebd02f8bec27b0bffb7a8de6
honam <138308525+Honam0905@users.noreply.github.com>
2024-05-29 09:32:46 +0700

    Update mod_mul.hpp

--- Mod/mod_mul.hpp
+++ Mod/mod_mul.hpp
@@ -1,10 +1,22 @@
 #pragma once
 #include "Modint/Barrett_reduction.hpp"
-long long modmul(long long a,long long b,long long mod){
+ll modmul(ll a,ll b,ll mod){
     Barrett br(mod);
     return br.mul(a, b);
 }
 
+ll mod_pow(ll a, ll n, ll mod) {
+  a %= mod;
+  Barrett bt(mod);
+  ll p = a;
+  ll v = 1;
+  while (n) {
+    if (n & 1) v = bt.mul(v, p);
+    p = bt.mul(p, p);
+    n >>= 1;
+  }
+  return v;
+}
 //or
 //only good for long long or int64_t
 long long modmul2(long long a,long long b,long long mod){
