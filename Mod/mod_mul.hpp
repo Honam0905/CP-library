fcaf548e432253b98803296e52b9462b5f91acf6
honam <138308525+Honam0905@users.noreply.github.com>
2024-05-29 23:07:58 +0700

    Update mod_mul.hpp

--- Mod/mod_mul.hpp
+++ Mod/mod_mul.hpp
@@ -1,21 +1,29 @@
 #pragma once
-ll modmul(ll a, ll b,ll mod) {
-    ll ans = 0;
-    while (b) {
-        if (b & 1)
-            (ans += a) %= mod;
-        (a *= 2) %= mod;
-        b /= 2;
+u64 get_nr(u64 M) {
+    u64 IV = 2 - M;
+    for (int i = 0; i < 5; ++i) {
+        IV *= 2 - M * IV;
     }
-    return ans;
+    return IV;
+}
+
+u64 modmul(u64 x, u64 y, u64 IV, u64 M) {
+    auto t = u128(x) * y;
+    u64 lo = t, hi = t >> 64;
+    return (hi + M) - u64((u128(lo * IV) * M) >> 64);
 }
  
-ll modpow(ll a, ll n,ll mod) {
-    ll ans = 1;
-    for (; n; n /= 2, a = modmul(a, a, mod))
-        if (n & 1)
-            ans = modmul(ans, a, mod);
-    return ans;
+u64 modpow(u64 a, u64 b, u64 M) {
+    u64 res = 1;
+    u64 IV = get_nr(M);
+    while (b) {
+        if (b & 1) {
+            res = mul(res, a, IV, M);
+        }
+        a = mul(a, a, IV, M);
+        b >>= 1;
+    }
+    return res;
 }
 //or
 //only good for long long or int64_t
