b31ac47b28dc32bdea1b1e29354466f88baaa754
honam <138308525+Honam0905@users.noreply.github.com>
2024-06-03 13:54:05 +0700

    Update primitive_root_yosupo.test.cpp

--- primitive_root_yosupo.test.cpp
+++ primitive_root_yosupo.test.cpp
@@ -1,20 +1,19 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/primitive_root"
 #include "Misc/marco.hpp"
 #include "Misc/debug.hpp"
-#include "Misc/faster_io.hpp"
 const int INF=1e9;
 const ll INFI=1e15;
 //----------Author: Nguyen Ho Nam,UIT, Saigon-----------------
 #include "Mod/Primitive_root.hpp"
 void solve()
 {
-   u64 n; sc.read(n);
-   pr.writeln(primitive_root(n));
+   u64 n; cin>>n;
+   cout<<primitive_root(n)<<'\n';
 }
 
 int main(){
 FT;
-int t; sc.read(t); int tc=0;
+int t; cin>>t; int tc=0;
 while(t--)
 {
 //tc++;
