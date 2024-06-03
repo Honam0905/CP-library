2669402ad5e5c9e7551f8248a9762e188f5d630d
honam <138308525+Honam0905@users.noreply.github.com>
2024-06-03 16:32:37 +0700

    Update Primitive_root.hpp

--- Mod/Primitive_root.hpp
+++ Mod/Primitive_root.hpp
@@ -1,24 +1,20 @@
+#include "NT/prime/factorize.hpp"
 #pragma once
-#include "Modint/Barrett_2.hpp"
-u64 primitive_root (u64 p) {
-    if(p==2) return 1;
-    vector<u64> fact;
-    Barrett_u64 br;
-    br.set(p);
-    u64 phi = p-1,  n = phi;
-    for (int i=2; i*i<=n; ++i)
-        if (n % i == 0) {
-            fact.push_back (i);
-            while (n % i == 0)
-                n /= i;
-        }
-    if (n > 1)
-        fact.push_back (n);
-    for (int res=2; res<=p; ++res) {
-        bool ok = true;
-        for (size_t i=0; i<fact.size() && ok; ++i)
-            ok &= pow_b64(&br,res, phi / fact[i]) != 1;
-        if (ok)  return res;
-    }
-    return -1;
+u64 PrimitiveRoot(u64 p){
+   u64 x=0;
+   auto mi=factor(p-1);
+   sort(all(mi));
+   Barrett_u64 br;
+   br.set(p);
+   mi.resize(unique(all(mi))-mi.begin());
+   for(int i=0;!i;){
+      i=1; ++x;
+      for(u64 f:mi){
+         if(pow_b64(&br,x,(p-1)/f)==1){
+            i=0; 
+            break;
+         }
+      }
+   }
+   return x;
 }
