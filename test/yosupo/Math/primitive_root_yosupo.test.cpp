1873bef975edd01c32fed188ee3c4deb357f3486
honam <138308525+Honam0905@users.noreply.github.com>
2024-06-03 13:03:14 +0700

    Update primitive_root_yosupo.test.cpp

--- test/yosupo/Math/primitive_root_yosupo.test.cpp
+++ test/yosupo/Math/primitive_root_yosupo.test.cpp
@@ -5,11 +5,15 @@ const int INF=1e9;
 const ll INFI=1e15;
 //----------Author: Nguyen Ho Nam,UIT, Saigon-----------------
 #include "Mod/Primitive_root.hpp"
+void solve(){
+    u64 n; cin>>n;
+    cout<<primitive_root(n)<<'\n';
+}
 int main() {
+    FT;
     int t; cin>>t;
     while(t--){
-     u64 n; cin>>n;
-     cout<<primitive_root(n)<<'\n';
+       solve();
     }
     return 0;
 }
