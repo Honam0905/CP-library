074cd2d427b27a24f4bf886d8d2a8cc2e24af8e8
honam <138308525+Honam0905@users.noreply.github.com>
2024-06-03 13:57:02 +0700

    Update primitive_root_yosupo.test.cpp

--- primitive_root_yosupo.test.cpp
+++ primitive_root_yosupo.test.cpp
@@ -5,20 +5,12 @@ const int INF=1e9;
 const ll INFI=1e15;
 //----------Author: Nguyen Ho Nam,UIT, Saigon-----------------
 #include "Mod/Primitive_root.hpp"
-void solve()
-{
-   u64 n; cin>>n;
-   cout<<primitive_root(n)<<'\n';
-}
-
-int main(){
-FT;
-int t; cin>>t; int tc=0;
-while(t--)
-{
-//tc++;
-//cout << "Case #" << tc << ": ";
-solve();
-// cerr << "Time elapsed: " << TIME << " s.\n";
-}
+int main() {
+    int t; cin>>t;
+    while(t--){
+        u64 n; cin>>n;
+        printf("%llu\n", primitive_root(n));
+    }
+    fflush(stdout); 
+    return 0;
 }
