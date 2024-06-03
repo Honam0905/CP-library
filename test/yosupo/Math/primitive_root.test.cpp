24b5520dc9344a1c777a92d4b4f455decfc5de60
honam <138308525+Honam0905@users.noreply.github.com>
2024-06-03 16:43:33 +0700

    Create primitive_root.test.cpp

---
+++ test/yosupo/Math/primitive_root.test.cpp
@@ -0,0 +1,16 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/primitive_root"
+#include "Misc/marco.hpp"
+#include "Misc/debug.hpp"
+const int INF=1e9;
+const ll INFI=1e15;
+//----------Author: Nguyen Ho Nam,UIT, Saigon-----------------
+#include "Mod/Primitive_root.hpp"
+
+int main() {
+    int t; cin>>t;
+    while(t--){
+      u64 n; cin>>n;
+      cout<<PrimitiveRoot(n)<<'\n';
+    }
+    return 0;
+}
